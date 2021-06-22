2c662744fe0fc21431bb4456ac048e201b0220e8
Syreeta Berry <mzberry1@hotmail.com>
2021-06-21 23:23:54 -0400

    updated

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -13,26 +13,45 @@ def scrape():
     html = browser.html
     mars_soup = soup(html, 'html.parser')
 
-    news_break = mars_soup.select_one('div.list_text')
-    data['title'] = news_break.find('div', class_='content_title').get_text()
-    data['paragraph'] = news_break.find('div', class_='article_teaser_body').get_text()
+    try:
+        news_break = mars_soup.select_one('div.list_text')
+        data['title'] = news_break.find('div', class_='content_title').get_text()
+        data['paragraph'] = news_break.find('div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
 
     url ='https://spaceimages-mars.com'
     browser.visit(url)
     html = browser.html
     image_soup = soup(html, 'html.parser')
-    image_url_rel = image_soup.find('img', class_='headerimage fade-in').get('src')
+    try:
+
+        image_url_rel = image_soup.find('img', class_='headerimage fade-in').get('src') 
+
+    except AttributeError:
+        return None
+
     data['image']= f'https://spaceimages-mars.com/{image_url_rel}'
-    
-    data['facts'] = pd.read_html('https://galaxyfacts-mars.com/')[0].to_html()
+     
+    try:
+        data['facts'] = pd.read_html('https://galaxyfacts-mars.com/')[0].to_html()
+
+    except BaseException:
+        return None
 
     browser.visit('https://marshemispheres.com/')
     hemispheres = []
     for i in range(4):
         hemisphere = {}
-        hemisphere['title'] = browser.find_by_css('a.itemLink h3')[i].text
-        browser.find_by_css('a.itemLink h3')[i].click()
-        hemisphere['url'] = browser.find_by_text('Sample')['href']
+        try:
+            hemisphere['title'] = browser.find_by_css('a.itemLink h3')[i].text
+            browser.find_by_css('a.itemLink h3')[i].click()
+            hemisphere['url'] = browser.find_by_text('Sample')['href']
+
+        except AttributeError:
+            return None, None
+
         browser.back()
         hemispheres.append(hemisphere)
     browser.quit()
